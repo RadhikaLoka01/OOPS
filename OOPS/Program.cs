11ef72e44f78810772c0bc878b19c3bd77fc922b
USER <USER@DESKTOP-D8A7NHR>
2024-06-06 16:37:51 +0530

    Add project files.

---
+++ OOPS/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS
+{
+    internal class Student
+    {
+        int sid = 101;
+        string sname = "Radha";
+        string scollege = "Prathibha";
+        string saddress = "Hyd";
+
+        internal void DisplaystudentData()
+        {
+            Console.WriteLine("Student id is {0}", sid);
+            Console.WriteLine("Student name is {0}", sname);
+            Console.WriteLine("Student college is {0}", scollege);
+            Console.WriteLine("Student address is {0}", saddress);
+        }
+    }
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+            s.DisplaystudentData();
+            Console.ReadLine();
+        }
+    }
+}
+
+
+            
+    
+    
+                
+
+
+           
+        
+    
+
+
+     
+
+        
+    
+
